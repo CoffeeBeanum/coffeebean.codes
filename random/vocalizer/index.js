c8e6197721d2aed93804707a05d46257b388b46e
McKiddo <compositedynamic@gmail.com>
2019-12-24 00:19:41 +0200

    Add missing sounds.

--- random/vocalizer/index.js
+++ random/vocalizer/index.js
@@ -1,54 +1,11 @@
-const audioFiles = {
-	A: 'vocal/a.mp3',
-	E: 'vocal/e.mp3',
-	I: 'vocal/i.mp3',
-	O: 'vocal/o.mp3',
-	U: 'vocal/u.mp3',
-	H: 'vocal/h.mp3',
-	L: 'vocal/l.mp3',
-	N: 'vocal/n.mp3',
-	G: 'vocal/g.mp3',
-	PAUSE: 'vocal/pause.mp3'
-}
-
 function getAudioFor(character) {
-	switch (character) {
-		case "a":
-			return new Audio(audioFiles.A);
-			break;
-		case "e":
-			return new Audio(audioFiles.E);
-			break;
-		case "i":
-			return new Audio(audioFiles.I);
-			break;
-		case "o":
-			return new Audio(audioFiles.O);
-			break;
-		case "u":
-			return new Audio(audioFiles.U);
-			break;
-		case "h":
-			return new Audio(audioFiles.H);
-			break;
-		case "l":
-			return new Audio(audioFiles.L);
-			break;
-		case "n":
-			return new Audio(audioFiles.N);
-			break;
-		case "g":
-			return new Audio(audioFiles.G);
-			break;
-		case " ":
-			return new Audio(audioFiles.PAUSE);
-			break;
-		case ".":
-			return new Audio(audioFiles.PAUSE);
-			break;
-		default:
-			return null;
+	if (character == " " || character == ".") {
+		character = "pause";
 	}
+	
+	let route = `vocal/${character}.mp3`;
+
+	return new Audio(route);
 }
 
 function playQueue(audioQueue) {
@@ -59,8 +16,6 @@ function playQueue(audioQueue) {
 
 function playAudio(index, audioQueue) {
 	index++;
-	
-	console.log(audioQueue[index]);
 	
     if (index == audioQueue.length) return;
 	
--- random/vocalizer/sound_loader.js
+++
@@ -1,7 +0,0 @@
-const vowels = {
-	A: new Audio('a.mp3');,
-	E: new Audio('e.mp3');,
-	I: new Audio('i.mp3');,
-	O: new Audio('o.mp3');,
-	U: new Audio('u.mp3');,
-}
