07516a89f1410e2ae4d6f74fbb2fcdac77135c87
Coffee Bean <compositedynamic@gmail.com>
2022-06-28 20:47:24 +0300

    [KidTech-Lite] Remove manual import to be added as a submodule

--- random/kidtech-lite/js/blockProperties.js
+++
@@ -1,3 +0,0 @@
-const transparentBlocks = [0, 2, 3, 5];
-const nonSolidBlocks = [0, 2, 9];
-const mergeableBlocks = [3];
--- random/kidtech-lite/js/constants.js
+++
@@ -1,4 +0,0 @@
-const piRatio = Math.PI / 180;
-const radians90Deg = 1.5708;
-
-export { piRatio, radians90Deg }
