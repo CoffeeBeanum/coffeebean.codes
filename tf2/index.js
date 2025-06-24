6fa5ab1ceeafcf53b9cf1ddf1df7b157a4005316
Coffee Bean <compositedynamic@gmail.com>
2025-06-24 21:30:26 +0300

    [TF2-MOTD] Update design, add new posts.

---
+++ tf2/index.js
@@ -0,0 +1,14 @@
+const tipTextBody = document.getElementById("tip-body");
+
+const adminTips = [
+    "Cheat and you're ass is bacon.",
+    "Be nice to new other players.",
+    "Focus on having fun."
+];
+
+function showRandomAdminTip() {
+    let index = Math.round(Math.random() * (adminTips.length - 1));
+    tipTextBody.innerText = adminTips[index];
+}
+
+showRandomAdminTip();
